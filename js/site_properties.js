b7fd0b2c11dfd6ea745f1079ecf5aaed815fdce4
Rachit Rathi <rachitrathi98@gmail.com>
2020-11-26 16:19:29 +0530

    [Rachit] Add. Employee Payroll App using JSON and Local Storage

---
+++ js/site_properties.js
@@ -0,0 +1,7 @@
+let site_properties = {
+    use_local_storage: "true",
+    home_page: "../pages/new_home.html",  
+    add_emp_payroll_page: "../pages/newEmpPayRoll.html",
+    server_url: "http://127.0.0.1:3000/EmployeePayrollDB/"   
+};
+
